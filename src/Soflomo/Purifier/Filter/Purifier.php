a53fbec652fa7c3a5e6844f0f86d2b395573d5b1
fgnas <fgnas@filmakademie.de>
2014-06-23 09:04:28 +0200

    Add possibility to declare the configuration for HTMLPurifier, set dynamically, as persistent or temporarily

--- src/Soflomo/Purifier/Filter/Purifier.php
+++ src/Soflomo/Purifier/Filter/Purifier.php
@@ -14,6 +14,13 @@ class Purifier implements FilterInterface
      */
     protected $purifierConfig;
 
+    /**
+     * @var bool
+     */
+    protected $persistentConf;
+
+
+
     public function __construct(HTMLPurifier $purifier)
     {
         $this->purifier = $purifier;
@@ -23,11 +30,33 @@ class Purifier implements FilterInterface
      * Sets config after initialization, that is used and passed to the
      * purify-method on filter() call
      * @param array $config
+     * @param bool  $persistentConf
      */
-    public function setConfig($config)
+    public function setConfig($config, $persistentConf = false)
     {
         $this->purifierConfig = $config;
+        $this->persistentConf = (bool) $persistentConf;
     }
+
+
+
+    /**
+     * Retrieves the configuration set after initialization, resets config if
+     * $this->persistentConf is set to FALSE (means in this case method returns
+     * NULL on second call).
+     * 
+     * @return array|null
+     */
+    public function getConfig()
+    {
+        $config = $this->purifierConfig;
+        if (!$this->persistentConf) {
+            $this->purifierConfig = null;
+        }
+    
+        return $config;
+    }
+
 
     protected function getPurifier()
     {
@@ -40,7 +69,7 @@ class Purifier implements FilterInterface
     public function filter($value)
     {
         if ($this->purifierConfig) {
-            return $this->getPurifier()->purify($value, $this->purifierConfig);
+            return $this->getPurifier()->purify($value, $this->getConfig());
         }
         return $this->getPurifier()->purify($value);
     }
