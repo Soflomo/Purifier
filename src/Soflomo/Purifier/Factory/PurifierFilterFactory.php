b5eca56fc934d3ab71ae40f2c4037ac2edecc31f
Jon Day <jon@switchsystems.co.uk>
2014-08-28 15:19:51 +0100

    Implemeted setting options for Purify Filter in factory

--- src/Soflomo/Purifier/Factory/PurifierFilterFactory.php
+++ src/Soflomo/Purifier/Factory/PurifierFilterFactory.php
@@ -3,18 +3,35 @@ namespace Soflomo\Purifier\Factory;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Soflomo\Purifier\Filter\Purifier;
 
 class PurifierFilterFactory implements FactoryInterface
 {
+    /**
+     * array of options for htmlpurify
+     * 
+     * @var array
+     */
+    protected $options;
+    
+    /**
+     * @param mixed $options
+     */
     public function __construct($options)
     {
-        exit(var_dump($options));
+        $this->options = $options;
     }
     
+    /**
+     * (non-PHPdoc)
+     * @see \Zend\ServiceManager\FactoryInterface::createService()
+     */
 	public function createService(ServiceLocatorInterface $serviceLocator)
 	{
 	    $purifier = $serviceLocator->getServiceLocator()->get('HTMLPurifier');
 	    
-	    return new Filter\Purifier($purifier);
+	    $filter = new Purifier($purifier);
+	    
+	    return $filter->setOptions($this->options);
 	}
 }
