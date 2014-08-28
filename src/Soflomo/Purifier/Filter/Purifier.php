7ec5d7764ca2c37347b0d8d3e657c4358d838ec3
Jon Day <jon@switchsystems.co.uk>
2014-08-28 15:27:27 +0100

    Adding the whitelist to html purify

--- src/Soflomo/Purifier/Filter/Purifier.php
+++ src/Soflomo/Purifier/Filter/Purifier.php
@@ -24,7 +24,9 @@ class Purifier extends AbstractFilter implements FilterInterface
     {
         if ($this->allowedClasses !== null) {
             $config = $this->purifier->config;
-            exit(var_dump($config));
+            $config->set('AllowedElements', $this->allowedClasses);
+            
+            //$this->purifier->config = $config;
         }
         
         return $this->purifier;
